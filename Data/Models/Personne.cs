4e64ee64f5d6cfbec6da80858f5b1628a0c7a4d1
Frédéric ROCHET <frederic.rochet@forem.be>
2018-08-08 18:36:24 +0200

    Sauvegarde de l'adresse

---
+++ Data/Models/Personne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+	public class Personne
+	{
+		public int id { get; set; }
+
+		public string Adresse { get; set; }
+	}
+}
