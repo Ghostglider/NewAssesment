2c9bd06439176cfad15e383ce5e2da9375a5128d
Frédéric ROCHET <frederic.rochet@forem.be>
2018-08-14 11:55:27 +0200

    Ajout d'un projet de TU

---
+++ TestAssesment/TestMappingPerson.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Assesment.Web.Mapping;
+using Assesment.Web.Models;
+using Data;
+using Data.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAssesment
+{
+	[TestClass]
+	public class TestMappingPerson
+	{
+		private readonly IMappingPerson _mappingPerson;
+
+		public TestMappingPerson()
+		{
+			_mappingPerson = new MappingPerson();
+		}
+		[TestMethod]
+		public void MapingCreatePersonVMToPersonTest()
+		{
+			//Arrange
+			CreatePersonVM createPersonVM = new CreatePersonVM();
+
+			createPersonVM.Name = "TestNom";
+			createPersonVM.FirstName = "testPrenom";
+			createPersonVM.Address = "testAdresse";
+
+			//Act
+			var result = _mappingPerson.MapingCreatePersonVMToPerson(createPersonVM);
+
+			//Assert
+			Assert.AreEqual(result.FirstName, createPersonVM.FirstName);
+			Assert.AreEqual(result.Id, 0);
+
+		}
+	}
+}
