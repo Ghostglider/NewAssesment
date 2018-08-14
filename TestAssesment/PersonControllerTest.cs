c13d059dbbe2fc77e16e12daed1a3d031230a200
Frédéric ROCHET <frederic.rochet@forem.be>
2018-08-14 13:24:48 +0200

    Test controller

---
+++ TestAssesment/PersonControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Data.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TestAssesment
+{
+	[TestClass]
+	public class PersonControllerTest
+	{
+		private readonly Mock<IPersonRepository> _personRepository;
+
+		public PersonControllerTest()
+		{
+			_personRepository = new Mock<IPersonRepository>;
+		}
+
+		[TestMethod]
+		public void GetPersonByIdTest()
+		{
+
+		}
+	}
+}
